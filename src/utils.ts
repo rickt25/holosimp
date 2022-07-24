e9b51798523684dde4ec3f23e8236605db01af8a
Elvin Valentino <elvin.valentino@gmail.com>
2022-07-24 22:11:21 +0700

    fix utils

--- src/utils.ts
+++ src/utils.ts
@@ -13,7 +13,7 @@ export const getTimeDifference = (from: Date): Date => {
 
 export const formatNumber = (num: number) => {
   if (num >= 1000000000) {
-    return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+    return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
   }
   if (num >= 1000000) {
     return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
